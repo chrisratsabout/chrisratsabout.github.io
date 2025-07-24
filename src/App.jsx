dbf80ab8273a603c134e5efa3097db85ecd533e2
Chris Ratsabout <chrisratsabout@gmail.com>
2025-07-24 09:40:22 -0400

    add hash router

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,32 @@ import AboutPage from "./pages/AboutPage";
 import ClientsPage from "./pages/ClientsPage";
 import ContactPage from "./pages/ContactPage";
 import ThanksPage from "./pages/ThanksPage";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <HashRouter>
         <Routes>
-          <Route path="/" element={<HomePage title={"Chris Ratsabout"}/>}></Route>
-          <Route path="/about" element={<AboutPage title={"About"}/>}></Route>
-          <Route path="/clients" element={<ClientsPage title={"Clients"}/>}></Route>
-          <Route path="/contact" element={<ContactPage title={"Contact"}/>}></Route>
-          <Route path="/thanks" element={<ThanksPage title={"Thanks"}/>}></Route>
+          <Route
+            path="/"
+            element={<HomePage title={"Chris Ratsabout"} />}
+          ></Route>
+          <Route path="/about" element={<AboutPage title={"About"} />}></Route>
+          <Route
+            path="/clients"
+            element={<ClientsPage title={"Clients"} />}
+          ></Route>
+          <Route
+            path="/contact"
+            element={<ContactPage title={"Contact"} />}
+          ></Route>
+          <Route
+            path="/thanks"
+            element={<ThanksPage title={"Thanks"} />}
+          ></Route>
         </Routes>
-
+      </HashRouter>
     </>
   );
 }
